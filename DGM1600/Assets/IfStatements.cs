bb55b259d3ac5bbdad0b67c71b91e722f8fb78be
Katie Cross <10740286@CS518-27.ad.uvu.edu>
2016-02-04 18:11:35 -0700

    variable function

---
+++ DGM1600/Assets/IfStatements.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatements : MonoBehaviour {
+
+	public float coffeeTemperature = 85.0f;
+	public float hotLimitTemperature = 70.0f;
+	public float coldLimitTemperature = 40.0f;
+
+
+	void Update ()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+			TemperatureTest();
+
+		coffeeTemperature -= Time.deltaTime * 5f;
+	}
+
+
+	void TemperatureTest()
+	{
+		if(coffeeTemperature > hotLimitTemperature)
+		{
+			Debug.Log("Coffee is too hot.");
+		}
+		else if(coffeeTemperature < coldLimitTemperature)
+		{
+			Debug.Log("Coffee is too cold.");
+		}
+		else
+		{
+			Debug.Log("Coffee is just right.");
+		}
+	}
+}
---
+++ DGM1600/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour {
+
+	int myInt = 5;
+	void Start () {
+
+		 MultiplyByFour (myInt);
+		Debug.Log (myInt);
+
+	}
+
+	int MultiplyByFour(int number){
+		int ret;
+
+		ret = number * 4;
+
+		return 10;
+	}
+
+
+}
