e6c630f2b078d6bc896a89c8141511d55efc1905
Zarathna <lipion97@gmail.com>
2016-04-28 23:30:49 -0600

    Midterm

---
+++ Midterm/Assets/PlayerControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+
+	public float rotationSpeed = 3;
+	public float movementSpeed = 20;
+	private Rigidbody player;
+
+	void Start()
+	{
+		player = GetComponent<Rigidbody>();
+		 
+	}
+
+	void Update()
+	{
+		if (Input.GetKey (KeyCode.LeftArrow))
+		{
+			transform.Rotate (new Vector3(0, -rotationSpeed, 0));
+		}
+		else if (Input.GetKey (KeyCode.RightArrow))
+		{
+				transform.Rotate (new Vector3(0, rotationSpeed, 0));
+		}
+		else if (Input.GetKey (KeyCode.UpArrow))
+		{
+			player.AddRelativeForce(0, 0, movementSpeed);
+		}
+	}
+}
---
+++ Midterm/Assets/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+	public float time = 5.0f;
+
+	void Update()
+	{
+		time -= Time.deltaTime;
+		if(time < 0.0f)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
